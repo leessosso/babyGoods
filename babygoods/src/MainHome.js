9efe990ad5ad62d371da0712e54fb50d6806a490
MyCodeIsYou <insonggod@naver.com>
2022-01-03 08:28:51 +0900

    Revert "메인에 소개페이지 링크 추가"
    This reverts commit 6f2f8acd191f744490c9f4583196f7288e9feeb2.

--- babygoods/src/MainHome.js
+++ babygoods/src/MainHome.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
 import styles from "./MainHome.module.css";
 import StyledComponent from "./StyledComponent";
 
@@ -8,9 +7,6 @@ const MainHome = () => {
     <Fragment>
       <div className={styles.wrapper}>
         유아용품 <span className="something">메인페이지</span>
-        <div>
-          <Link to="/introduce">사이트 소개</Link>
-        </div>
       </div>
       <div>
         <StyledComponent />
