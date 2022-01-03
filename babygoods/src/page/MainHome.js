84e21bdffed8f94ecdc0a35be2a89c002d251216
MyCodeIsYou <insonggod@naver.com>
2022-01-03 13:47:30 +0900

    [inseong-0103-2] image 추가 하기

--- babygoods/src/page/MainHome.js
+++ babygoods/src/page/MainHome.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from "react";
+import MainImage from "../components/Base/MainImage/MainImage";
+import Logo from "../components/Image/Loge";
 import styles from "./MainHome.module.css";
-import StyledComponent from "../StyledComponent";
+// import StyledComponent from "../StyledComponent";
 
 const MainHome = () => {
   const [value, setValue] = React.useState("one");
@@ -15,7 +17,9 @@ const MainHome = () => {
         유아용품 <span className="something">메인페이지</span>
       </div>
       <div>
-        <StyledComponent />
+        <MainImage>
+          <Logo />
+        </MainImage>
       </div>
     </Fragment>
   );
