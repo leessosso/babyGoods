67463d0990af47c7bbb24bd9dd5ff0ceafd2b4e1
이승석 <ssossoking@naver.com>
2022-01-12 14:51:30 +0900

    서버 구현 작업 220112

--- babygoods/src/App.js
+++ babygoods/src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MainHome from "./page/MainHome";
 import { Route, Routes } from "react-router-dom";
 import Introduce from "./page/Introduce";
 import Crwalling from "./page/Crwalling";
 import HeaderContainer from "./containers/Base/HeaderContainer";
 import MenuBarContainer from "./containers/Base/menuBarContainer";
+import axios from "axios";
+
 const App = () => {
+  const [testState, setTestState] = useState("nothing");
+  const callApi = async () => {
+    axios.get("/api").then((res) => {
+      console.log(res.data.test);
+      setTestState(res.data.test);
+    });
+  };
+
+  useEffect(() => {
+    callApi();
+  }, []);
+
   return (
     <div>
       <HeaderContainer />
@@ -17,6 +31,7 @@ const App = () => {
         <Route path="/introduce" element={<Introduce />} />
         <Route path="/crwalling" element={<Crwalling />} />
       </Routes>
+      <div>{testState}</div>
       {/* <MainHome /> */}
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,20 +1,19 @@
 // node_modules 에 있는 express 관련 파일을 가져온다.
-var express = require("express");
-
-const path = require("path");
-
+const express = require("express");
 // express 는 함수이므로, 반환값을 변수에 저장한다.
-var app = express();
+const app = express();
+const path = require("path");
+const port = 3001;
 
-// 3000 포트로 서버 오픈
-app.listen(8080, function () {
-  console.log("start! express server on port 3000");
+const test = require("./Router/test");
+// 3001 포트로 서버 오픈
+app.listen(port, function () {
+  console.log(`start! express server on port ${port}`);
 });
 
-// 이제 터미널에 node app.js 를 입력해보자.
+app.use("/api", test);
 
 app.use(express.static(path.join(__dirname, "../babygoods/build")));
-
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../babygoods/build/index.html"));
 });
